3b14bf598265a8ccb56f85f3e24f641acea315ce
CblPOCHEK <sasadevatvoskij@gmail.com>
2024-05-24 07:10:00 +0300

    2

--- main.py
+++ main.py
@@ -2,13 +2,21 @@ from aiogram import Bot, Dispatcher, types, executor
 from config import TELEGRAM_TOKEN
 from keyboard.keyboard import get_keyboard_1, get_keyboard_2
 from keyboard.key_inline import get_keyboard_inline
+from database.database import initialize_db,add_user,get_user
 
 bot = Bot (token = '6764754713:AAH4ZZeZkJUytUAszB1ATts9wu79VB7250M')
 dp = Dispatcher (bot)
 
+initialize_db()
+
 @dp.message_handler (commands= 'start')
-async def start(message: types.Message) :
-    await message.answer('Привет, я твой первый ЭХО бот!',reply_markup=get_keyboard_1())
+async def start(message: types.Message):
+    user = get_user(message.from_user.id)
+    if user is None:
+        add_user(message.from_user.id, message.from_user.username, message.from_user.first_name, message.from_user.last_name)
+        await message.answer('Привет, я твой первый ЭХО бот!',reply_markup=get_keyboard_1())
+    else:
+        await message.answer('Привет, я твой первый ЭХО бот!', reply_markup=get_keyboard_1())
 
 @dp.message_handler (lambda message: message.text == 'Отправь фото кота')
 async def button_1_click(message: types.Message) :
